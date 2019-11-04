eaf45ab4795195f5e8e231d76c4983338d56c865
Ayush Gupta <7ayushgupta@gmail.com>
2019-11-05 01:31:40 +0530

    COmpleted spanning trees

--- PA4/spanningTrees.cpp
+++ PA4/spanningTrees.cpp
@@ -15,15 +15,20 @@ class Graph
 {
      public:
      ll V, E;
-     edge* edges;   
+     edge* edges, *spanningTree;
+     ll *parents, *ranks;
      Graph(ll, ll);    
      void performInput();
      void sortEdges(ll, ll);
      void mergeEdges(ll, ll, ll);
      void reallocEdges(edge);
      ll KruskalMST(); 
+     void unionSet(ll, ll, ll*, ll*);
+     void link(ll, ll, ll*, ll*);
      ll findSet(ll *, ll);
      void simpleSort();
+     ll makePartition(ll, ll);
+     void quickSort(ll, ll);
      void printEdges();
 };
 
@@ -37,15 +42,16 @@ void Graph::printEdges()
 Graph::Graph(ll v, ll e)
 {
     edges = (edge *) malloc(e*sizeof(edge));
+    parents = (ll*) malloc(v*sizeof(ll));
+    ranks = (ll*) malloc(v*sizeof(ll));
+    spanningTree = (edge *)malloc(sizeof(edge)*(this->V));
     V=v; E=e;
 }
 
 void Graph::performInput()
 {
     for(int i = 0; i<this->E; i++)
-        cin>>edges[i].u>>edges[i].v>>edges[i].weight;
-        // scanf("%lld %lld %lld", &(edges[i]).u, &(edges[i]).v, &(edges[i]).weight);
-
+        scanf("%lld %lld %lld", &(edges[i]).u, &(edges[i]).v, &(edges[i]).weight);
 }
 
 void Graph::sortEdges(ll m, ll n)
@@ -62,10 +68,10 @@ void Graph::sortEdges(ll m, ll n)
 
 void Graph::simpleSort()
 {
-    edge* updated_edges = (edge *) malloc((this->E)*sizeof(edge));
+    edge updated_edges[this->E];
     edge new_edge = edges[this->E-1];
     ll position;
-    for(ll i = 0; i<this->E-2; i++)
+    for(ll i = 0; i<=this->E-2; i++)
         if(edges[i].weight>new_edge.weight)
         {    
             position=i;
@@ -73,13 +79,11 @@ void Graph::simpleSort()
         }
     for(ll i = 0; i<position; i++)
         updated_edges[i] = edges[i];
-    
     updated_edges[position] = new_edge;
-    
     for(ll i = position+1; i<this->E; i++)
         updated_edges[i] = edges[i-1];
-    
-    edges = updated_edges;
+    for(int i = 0; i<this->E; i++)
+        edges[i]=updated_edges[i];
 }
 
 void Graph::mergeEdges(ll p, ll q, ll r)
@@ -101,51 +105,43 @@ void Graph::mergeEdges(ll p, ll q, ll r)
             this->edges[i] = rightPortion[b++];
     }
     return;
-    
 }
 
-ll Graph::findSet(ll *sets, ll a)
-{
-    if(sets[a]==-__INT_MAX__) 
-        return a;
-    return findSet(sets, sets[a]);
-}
+ll Graph::makePartition(ll low, ll high)  
+{  
+    edge pivot = edges[high];
+    ll i=low-1; 
+    for (ll j = low; j <= high - 1; j++)  
+    {  
+        if (edges[j].weight < pivot.weight)  
+        {  
+            i++;   
+            edge temp = edges[i];
+            edges[i] = edges[j];
+            edges[j] = temp;
+        }  
+    }  
+    edge temp = edges[i+1];
+    edges[i+1] = edges[high];
+    edges[high] = temp;
+    return (i+1);  
+}  
 
-ll Graph::KruskalMST()
+void Graph::quickSort(ll low, ll high)
 {
-    ll result = 0;
-    ll* sets = (ll *) malloc(sizeof(ll)*this->V);
-    for (ll i = 0; i<this->V; i++)
-        sets[i]=-__INT_MAX__;
-    edge* spanningTree = (edge*)malloc(this->V*sizeof(edge));
-    for (ll i = 0, edgesAdded=0; i<this->E; i++)
+    if (low<high)
     {
-        if(edgesAdded==this->V-1)
-            break;
-        ll findSetA = findSet(sets, this->edges[i].u);
-        ll findSetB = findSet(sets, this->edges[i].v);
-        if(findSetA == findSetB)
-            continue;
-        spanningTree[edgesAdded] = edges[i];
-        edgesAdded++;
-        result+=edges[i].weight;
-        ll unionSetA = findSet(sets, findSetA);
-        ll unionSetB = findSet(sets, findSetB);
-        if(unionSetA!=unionSetB)
-            sets[unionSetA]=unionSetB;
+        ll pivotReturned = Graph::makePartition(low, high);
+        quickSort(low, pivotReturned-1);  
+        quickSort(pivotReturned+1, high); 
     }
-    edges = spanningTree;
-    this->E=this->V;
-    free(sets);
-    return result;
 }
 
 void Graph::reallocEdges(edge new_edge)
 {   
-    edge* updated_edges = (edge *) malloc((this->E)*sizeof(edge));
-    // edge new_edge = edges[this->E-1];
+    edge updated_edges[this->E];
     ll position;
-    for(ll i = 0; i<this->E-2; i++)
+    for(ll i = 0; i<=this->E-2; i++)
         if(edges[i].weight>new_edge.weight)
         {    
             position=i;
@@ -153,47 +149,81 @@ void Graph::reallocEdges(edge new_edge)
         }
     for(ll i = 0; i<position; i++)
         updated_edges[i] = edges[i];
-    
     updated_edges[position] = new_edge;
-    
     for(ll i = position+1; i<this->E; i++)
-        updated_edges[i] = edges[i-1];
+        updated_edges[i] = edges[i-1];    
+    for(int i = 0; i<this->E; i++)
+        edges[i]=updated_edges[i];
+}
+
+ll Graph::findSet(ll *parents, ll x)
+{
+    if(x!=parents[x])
+        parents[x]=findSet(parents, parents[x]);
+    return parents[x];
+}
+
+void Graph::link(ll x, ll y, ll *ranks, ll *parents)
+{
+    if(ranks[x]>ranks[y])
+        parents[y]=x;
+    else
+    {
+        parents[x]=y;
+        if(ranks[x]==ranks[y])
+            ranks[y]++;
+    }
     
-    edges = updated_edges;
-    
-//     this->E++;
-//     edge* updated_edges = (edge *) malloc((this->E)*sizeof(edge));
-//     for(int i = 0; i<this->E-1; i++)
-//         updated_edges[i] = edges[i];
-//     updated_edges[this->E-1] = new_edge;
-//     edge* old_edges = edges;
-//     edges = updated_edges;
-//     free(old_edges);
-//     return;
+}
+
+void Graph::unionSet(ll x, ll y, ll* ranks, ll* parents)
+{
+    link(findSet(parents, x), findSet(parents, y), ranks, parents);
+}
+
+ll Graph::KruskalMST()
+{
+    // printEdges();
+    ll result = 0;
+    for(ll i = 0; i<this->V; i++)
+    {
+        parents[i]=i;
+        ranks[i]=0;
+    }
+    for (ll i = 0, edgesAdded=0; i<this->E; i++)
+    {
+        ll findSetA = findSet(parents, this->edges[i].u);
+        ll findSetB = findSet(parents, this->edges[i].v);
+        if(findSetA!=findSetB)
+        {
+            spanningTree[edgesAdded] = edges[i];
+            edgesAdded++;
+            result += this->edges[i].weight;
+            if(edgesAdded==this->V-1)
+                break;
+            unionSet(this->edges[i].u, this->edges[i].v, ranks, parents);
+        }
+    }
+    edges=spanningTree;
+    return result;
 }
 
 int main() 
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
     ll v, e, q;
-    cin>>v>>e>>q;
-    // scanf("%lld %lld %lld\n", &v, &e, &q);
+    scanf("%lld %lld %lld\n", &v, &e, &q);
     Graph graph(v, e);
     graph.performInput();
     graph.sortEdges(0, e-1);
-    cout<<graph.KruskalMST()<<endl;
-    // printf("%lld\n", graph.KruskalMST());
-    for(int i = 0; i<q; i++)
+    printf("%lld\n", graph.KruskalMST());
+    graph.E=graph.V;
+    
+    for(ll i = 0; i<q; i++)
     {
         edge new_edge;
-        // scanf("%lld %lld %lld", &(new_edge).u, &(new_edge).v, &(new_edge).weight);
-        cin>>new_edge.u>>new_edge.v>>new_edge.weight;
+        scanf("%lld %lld %lld", &(new_edge).u, &(new_edge).v, &(new_edge).weight);
         graph.reallocEdges(new_edge);
-        // graph.simpleSort();
-        cout<<graph.KruskalMST()<<endl;
-        // printf("%lld\n", graph.KruskalMST());
+        printf("%lld\n", graph.KruskalMST());
     }
     return 0;
 }
