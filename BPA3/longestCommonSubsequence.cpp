4f96af090fd1a46e0cbfb8273915f22286138ea6
Ayush Gupta <7ayushgupta@gmail.com>
2019-11-09 20:08:34 +0530

    Completed 1st question of BPA3

--- BPA3/longestCommonSubsequence.cpp
+++ BPA3/longestCommonSubsequence.cpp
@@ -5,23 +5,26 @@
 using namespace std;
 typedef long long int ll;
   
-ll computeLCS(char *string_a, char *string_b, ll m, ll n)  
+ll computeLCS(char *string_a, char *string_b, ll length_1, ll length_2)  
 {  
-    int table[m+1][n+1];  
-    for (int i = 0; i <= m; i++)  
+    int dp_previous[length_2+1] = {0};
+    int dp_current[length_2+1] = {0};
+    for (int i = 0; i <= length_1; i++)  
     {  
-        for (int j = 0; j <= n; j++)  
+        for (int j = 0; j <= length_2; j++)  
         {  
             if (i == 0 || j == 0)  
-                table[i][j] = 0;  
-            else if (string_a[i - 1] == string_b[j - 1])  
-                table[i][j] = table[i - 1][j - 1] + 1;  
+                dp_current[j]=0;
+            else if (string_a[i-1] == string_b[j-1])  
+                dp_current[j] = dp_previous[j-1]+1;
         
             else
-                table[i][j] = max(table[i - 1][j], table[i][j - 1]);  
+                dp_current[j] = max(dp_current[j-1], dp_previous[j]);
         }  
+        for(int j=0;j<=length_2;j++)
+            dp_previous[j] = dp_current[j];
     }  
-    return table[m][n];  
+    return dp_current[length_2];
 }  
 
 int main() {
@@ -34,7 +37,7 @@ int main() {
         char string_1[length1+1];
         char string_2[length2+1];
         scanf("%s\n%s", string_1, string_2);
-        printf("%lld", computeLCS(string_1, string_2, length1, length2));
+        printf("%lld\n", computeLCS(string_1, string_2, length1, length2));
     }
     return 0;
 }
